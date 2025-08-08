27de59c032210a30a54a1166c313a9e2f38965b1
Matheus Isaias <140298484+MeloIsaiazz@users.noreply.github.com>
2025-08-08 16:22:12 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 Curso do Felipe Dechamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Ludmyla, eu amo você muuuuuuito!!! Se você me ama, manda um emoji de coração. 💖🫶</h1>
+  return (
+    <h1>
+      Ludmyla, eu amo você muuuuuuito!!! Se você me ama, manda um emoji de
+      coração. 💖🫶
+    </h1>
+  );
 }
 
-export default Home
+function Random() {
+  return <h1>Random</h1>;
+}
+
+export default Home;
