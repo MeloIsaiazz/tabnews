816c3c451986c0769227621c09e6e459afab60a2
Matheus Isaias <140298484+MeloIsaiazz@users.noreply.github.com>
2025-08-18 04:07:01 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da media." });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "Valor" });
 }
 
 export default status;
