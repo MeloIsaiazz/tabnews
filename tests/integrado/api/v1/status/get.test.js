dd617cec226aa4f54593c3f4e57e0bee62c71225
Matheus Isaias <140298484+MeloIsaiazz@users.noreply.github.com>
2025-08-22 21:21:13 +0000

    finish `/status` endpoint

--- tests/integrado/api/v1/status/get.test.js
+++ tests/integrado/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
+  // Se o endpoint está retornando status 200
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Se o endpoint está retornando uma data
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  console.log(responseBody);
+
+  // Se o endpoint retorna uma data válida
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
